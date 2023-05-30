82bb4e4f232dac856ea4a13f2fabd1eac577a818
hdt003 <harshalthakor003@gmail.com>
2023-05-30 18:40:11 +0530

    app file added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+import { auth } from './firebase';
+import { useEffect,useState } from 'react';
 function App() {
+  const [Userid,setUserid]=useState("")
+  const [mode,setMode]=useState("light");
+  const [aalert,setAlert]=useState("Light Mode Enabled")
+
+  useEffect(()=>{ 
+    
+    auth.onAuthStateChanged((user)=>{
+      // console.log("user:",user)
+      if(user){
+        setUserid(user.uid)
+      }
+    });
+    togglemode();
+  },[])
+
+      //to change mode
+      const togglemode=()=>{
+        
+        if(mode==="dark")
+        {
+          setMode("light")
+            // document.body.style.backgroundColor="rgb(45,82,71)";
+            document.body.style.backgroundImage="none"
+            document.body.style.backgroundImage="linear-gradient(90deg, #3EADCF 0%, #ABE9CD 100%)";//blue //rgb(71, 194, 255)"//linear-gradient(90deg, #ff4800 0%, #dd2476 100%)
+            document.body.style.color="black"
+            setAlert("dark mode Enabled")
+            // console.log("light")
+            document.title="Menu App - Light mode"
+        }
+         if(mode==="light")
+        {
+          setMode("dark")//linear-gradient(to right, #0000ff, #add8e6)
+          document.body.style.backgroundImage = "none";
+          document.body.style.backgroundColor="rgb(48, 51, 51)";//black
+          document.body.style.color="white"
+          setAlert("Light mode Enabled")
+          // console.log("dark")
+          document.title="Menu App - Dark mode"
+        }
+    }
+
+    var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+    
+    function alertfunc() {
+      var wrapper = document.createElement('div')
+      wrapper.innerHTML = '<br/><div class="alert alert-success alert-dismissible text-dark text-center border-2 border-dark" role="alert">' + aalert + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+      alertPlaceholder.appendChild(wrapper)
+      clc()
+    }
+
+  //to clear alert box every 5 seconds
+  function clc(){
+  setTimeout(() => {
+    // clearBox()
+    document.getElementById('liveAlertPlaceholder').innerHTML = "";
+  }, 5000);
+} 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div>
+    <Router>
+      <hr style={{width:"10px"}}/>
+
+    <div className='mt-5 container ' id="liveAlertPlaceholder"></div>
+    {/* <br/> */}
+        <Routes>
+            <Route exact path="/" element={<Area uid={Userid } value={timeBasedFood(new Date)} field="type" pagename="home" mode={mode}/> }/>
+       
+            <Route exact path="/login" element={<Login mode={mode}/>}/>
+            <Route exact path="/SignUp" element={<SignUp mode={mode}/>}/>
+            <Route exact path="/Profile" element={<Profile uid={Userid } mode={mode}/>}/>
+        </Routes>
+      </Router>
+      </div>
+    </>
   );
 }
 
